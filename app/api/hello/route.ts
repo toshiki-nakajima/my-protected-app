f417337a373726557d787bb9cc637903a7c372ae
toshiki-nakajima <72725615+toshiki-nakajima@users.noreply.github.com>
2025-03-15 16:43:18 +0900

    get log

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from 'next/server';
 
 export async function GET(request: NextRequest) {
+    console.log('GET request received');
     return NextResponse.json({message: 'Hello API'});
 }
 
