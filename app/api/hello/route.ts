3a9d5334213129d9d5d6b0757ef1ba6556496fbd
toshiki-nakajima <72725615+toshiki-nakajima@users.noreply.github.com>
2025-03-15 16:47:45 +0900

    post_to_open_api

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -21,7 +21,35 @@ export async function POST(request: NextRequest) {
     console.log('POST request received');
     try {
         const body = await request.json();
+        const openApiBaseUrl= process.env.OPEN_API_BASE_URL;
+        const openApiPathOfAction= process.env.OPEN_API_ACTION_PATH;
+        const openApiCode = process.env.OPEN_API_CODE
+        const openApiTriggerOfAddingPoints = process.env.OPEN_API_TRIGGER_OF_ADDING_POINTS;
+        const postUrl = `${openApiBaseUrl}/${openApiPathOfAction}/${openApiCode}/${openApiTriggerOfAddingPoints}`;
+        const openApiToken = process.env.OPEN_API_TOKEN;
         // Process the data
+        const {uid, points} = body as RequestedBodyFromClient;
+        const bodyToRequest: BodyToRequestForOpenApi = {
+            uid,
+            params: {
+                points
+            }
+        }
+
+        await fetch(postUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${openApiToken}`
+            },
+            body: JSON.stringify(bodyToRequest)
+        }).then(res => res.json()).then(
+            (res) => {
+                console.log(res);
+            }
+        ).catch(err => console.log(err));
+
+
         return NextResponse.json({
             success: true,
             data: body,
